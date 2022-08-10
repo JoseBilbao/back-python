2e7015aa20ae9f7120caab4ff26e19161e19b7c1
zxin007 <zixin@nekki.cl>
2022-08-10 13:05:32 -0400

    culture

--- Culture/src/predict.py
+++ Culture/src/predict.py
@@ -28,7 +28,7 @@ for emp in employees.to_dict('records'):
         'male':emp['male'],
         'rating':emp['rating'],
         'branch_id':d,
-        # 'axl': emp['acquired']*d
+        'axl': emp['acquired']*d
     }
 
 retention = data.drop(columns=['Unnamed: 0','month','diff','n_emails','innov'])
@@ -55,6 +55,7 @@ for emp in employees_dict:
         df.append(employees_dict[emp])
 
 df = pd.DataFrame(df)
+
 df.fillna(0, inplace=True)
 X_train, X_test= train_test_split(df, test_size=0.33, random_state=42)
 
@@ -62,14 +63,14 @@ retentions_X_train = X_train.drop(columns=['innov','name'], axis=1)
 reformations_X_train = X_train.drop(columns=['ret','name'], axis=1)
 
 retentions_X_train = sm.add_constant(retentions_X_train) 
-
+print(retentions_X_train)
 retentions_y_train = X_train['ret']
 reformations_y_train = X_train['innov']
 
-olsmod = smf.ols(formula='ret ~ acquired + tenure + title_status + male + rating + branch_id', data=retentions_X_train)
+olsmod = smf.ols(formula='ret ~ acquired + tenure + title_status + male + rating + branch_id + axl', data=retentions_X_train)
 olsres = olsmod.fit()
 
-olsmod_innov = smf.ols(formula='innov ~ acquired + tenure + title_status + male + rating + branch_id', data=reformations_X_train)
+olsmod_innov = smf.ols(formula='innov ~ acquired + tenure + title_status + male + rating + branch_id + axl', data=reformations_X_train)
 olsres_innov = olsmod_innov.fit()
 
 def prediction(data):
@@ -80,7 +81,7 @@ def prediction(data):
     data["rating"] = data.rating.astype(float)
     data["male"] = data.male.astype(int)
     data["branch_id"] = data.branch_id.astype(float)
-
+    
     ypred = olsres.predict(data)
     ypred_innov = olsres_innov.predict(data)
     return {'ret':ypred[0], 'innov':ypred_innov[0]}
